8a37b224450802368b058efc1aaf09d9c90310e6
zswang <wjhu111@21cn.com>
2015-05-16 19:45:50 +0800

    添加代码结构

--- src/jcuts.js
+++ src/jcuts.js
@@ -4,6 +4,13 @@
 
     var exports = exports || {};
 
+    /**
+     * 判断点是否在多边形中
+     *
+     * @param {Array} p 点坐标
+     * @param {Array} polygon 多边形坐标
+     * @return {boolean} 返回点是否在多边形中
+     */
     function ptInPolygon(p, polygon) {
         var cross = 0;
         for (var i = 0; i < polygon.length; i++) {
@@ -65,6 +72,7 @@
         var similarity = 1 - calculateDiff(canvas1, canvas2, width, height);
         return similarity;
     }
+    exports.diffPolygon = diffPolygon;
 
     /**
      * 获得完整剪纸的多边形
@@ -100,7 +108,8 @@
                 beginX = x;
                 beginY = y;
                 context.moveTo(x, y);
-            } else {
+            }
+            else {
                 context.lineTo(x, y);
             }
         }
@@ -124,7 +133,7 @@
         var imgData2 = context2.getImageData(0, 0, width, height);
         var diffPixelNum = 0;
         var baseNum = 0;
-        for (var i = 0; i < imgData1.data.length; i+=4) {
+        for (var i = 0; i < imgData1.data.length; i += 4) {
             baseNum += imgData1.data[i + 3] / 255;
             baseNum += imgData2.data[i + 3] / 255;
             var pixelDiff = Math.abs(imgData1.data[i + 3] - imgData2.data[i + 3]) / 255;
@@ -132,7 +141,6 @@
         }
         return diffPixelNum / baseNum;
     }
-
     exports.diffPolygon = diffPolygon;
 
     /**
@@ -255,10 +263,13 @@
             cutModelPath.forEach(function(line) {
                 m.push([line[0].slice(), line[1].slice()]);
             });
-            return format('M #{start} L #{lines}', {
-                start: m[0][0],
-                lines: m.join(' ')
-            });
+            return format('M #{start} L #{lines}' +
+                'M #{start} m -5,0 h 10 M #{start} m 0,-5 v 10' +
+                'M #{end} m -8,0 h 16 M #{end} m 0,-8 v 16', {
+                    start: m[0][0],
+                    end: m[m.length - 1][1],
+                    lines: m.join(' ')
+                });
         }
         instance.getCutModelPath = getCutModelPath;
 
@@ -270,7 +281,7 @@
          */
         function cut(polygon) {
             if (!polygon || polygon.length < 2) {
-                // console.log('cut exit 1');
+                console.log('cut exit 1');
                 return;
             }
 
@@ -278,7 +289,7 @@
             var modelPolygon = getModelPolygon();
             if (ptInPolygon(polygon[0], modelPolygon) ||
                 ptInPolygon(polygon[polygon.length - 1], modelPolygon)) {
-                // console.log('cut exit 2');
+                console.log('cut exit 2');
                 return;
             }
 
@@ -289,9 +300,26 @@
                     var lineB = [polygon[j - 1], polygon[j]];
                     if (jmaths.doubleLineIntersect(
                         lineA[0], lineA[1], lineB[0], lineB[1])) {
-                        // console.log('cut exit 3');
+                        console.log('cut exit 3');
                         return;
                     }
+                }
+            }
+
+            function doCut(startModelIndex, endModelIndex) {
+                var start = cutModelPath[0][0];
+
+                // var a = [];
+                // var b = [cutModelPath[cutModelPath.length - 1]];
+                //
+
+                for (var i = 0; i < modelPath.length; i++) {
+
+                    var modelIndex = (startModelIndex + i) % modelPath.length;
+
+                    // var model = modelPath[];
+
+                    // a.push();
                 }
             }
 
@@ -304,13 +332,16 @@
                     next = polygon[i];
 
                     for (var j = 0; j < modelPath.length; j++) {
-                        var lineB = modelPath[(modelIndex + j) % modelPath.length];
-                        var intersect = jmaths.doubleLineIntersect(
-                            start, next, lineB[0], lineB[1]);
-                        if (intersect) {
-                            cutModelPath.push([start, intersect, 'scar']);
+                        var line = modelPath[j];
+                        if (i === polygonIndex && modelIndex === j) {
+                            continue;
+                        }
+                        var p = jmaths.doubleLineIntersect(
+                            start, next, line[0], line[1]);
+                        if (p) {
+                            cutModelPath.push([start, p, 'scar']);
 
-                            console.log(intersect, cutModelPath);
+                            doCut(modelIndex, j);
                             return;
                         }
                     }
@@ -324,30 +355,97 @@
                 var next = polygon[i];
 
                 for (var j = 0; j < modelPath.length; j++) {
-                    var lineB = modelPath[j];
+                    var line = modelPath[j];
                     var intersect = jmaths.doubleLineIntersect(
-                        start, next, lineB[0], lineB[1]);
-
+                        start, next, line[0], line[1]);
                     if (intersect) { // 出现相交
                         cutSub(intersect, i, j);
-                        return;
+                        // console.log(JSON.stringify(polygon));
+                        return true;
                     }
                 }
                 start = next;
             }
-
-            return true;
         }
 
         instance.cut = cut;
 
         instance.getModelPath = getModelPath;
 
+        return instance;
+    }
+    exports.createPaper = createPaper;
+
+    /**
+     * 创建剪纸游戏实例
+     *
+     * @param {Object} options 配置项
+     * @param {Element|string} options.container 容器，选择器或者元素对象
+     * @param {=number} options.edges 边数，3~6，默认 6
+     * @param {=Function} options.onchange 用户剪切纸张
+     * @return {Object} 返回游戏实例
+     */
+    function createGame(options) {
+        console.log('createGame');
+        /**
+         * 返回当前用户剪辑留下的形状
+         *
+         * @return {Array} 返回形状路径数据
+         */
+        instance.getPolygon = function() {
+            console.log('getPolygon()');
+        };
+
+        /**
+         * 返回当前用户剪辑掉落的形状
+         *
+         * @return {Array} 返回形状路径数据
+         */
+        instance.getCutPolygon = function() {
+            console.log('getCutPolygon()');
+        };
+
+        /**
+         * 开始游戏
+         */
+        instance.replay = function() {
+            console.log('replay()');
+        };
+
+        /**
+         * 停止游戏
+         */
+        instance.stop = function() {
+            console.log('stop()');
+        };
+
+        /**
+         * 释放游戏资源
+         */
+        instance.free = function() {
+            console.log('free()');
+        };
 
         return instance;
     }
 
-    exports.createPaper = createPaper;
+    /**
+     * 渲染剪纸效果
+     *
+     * @param {Element|string} container 渲染容器，元素或选择器，得固定高宽
+     * @param {number} edges 边数，3~6
+     * @param {Array} polygon 多边形
+     * @return {Object} 渲染示例
+     */
+    function renderPager(container, edges, polygon) {
+        console.log('renderPager');
+        /**
+         * 释放游戏资源
+         */
+        instance.free = function() {
+           console.log('free');
+        };
+    }
 
     if (typeof define === 'function') {
         if (define.amd || define.cmd) {
